da6eb218dfd8591c3c75ea608f4187380767b7d0
Joseph Gonzalez <josephgonowak97@gmail.com>
2021-12-15 00:31:16 -0400

    improving the defined translation

--- tensordb/tensor_translator.py
+++ tensordb/tensor_translator.py
@@ -3,7 +3,9 @@ import dask
 import numpy as np
 import itertools
 
-from typing import Iterable, Callable, Union, Dict, Any, List, Tuple, Optional, Literal, Hashable
+from typing import Iterable, Callable, Union, Dict, Any, List, Tuple, Optional, Literal, Hashable, Generator
+from math import ceil
+
 from pydantic import validate_arguments
 from loguru import logger
 
@@ -68,37 +70,102 @@ def defined_translation(
 
     """
 
-    as_data_array = False
-    if data_names is None:
-        data_names = ['null']
-        as_data_array = True
+    as_data_array = data_names is None
 
-    if not isinstance(dtypes, list):
-        dtypes = [dtypes]
-
-    if len(dtypes) != len(data_names):
+    if not as_data_array and len(dtypes) != len(data_names):
         raise ValueError(
             f'The number of dtypes ({len(dtypes)}) does not match the number of dataset names ({len(data_names)}), '
             f'you need to specify a dtype for every data array in your dataset'
         )
 
-    total_chunk_coords = []
-    for chunk, dim in zip(chunks, dims):
-        coord = coords[dim]
-        chunk = len(coord) if chunk is None else chunk
-        total_chunk_coords.append([coord[i: i + chunk] for i in range(0, len(coord), chunk)])
+    chunks = [len(coords[dim]) if chunk is None else chunk for chunk, dim in zip(chunks, dims)]
 
-    func_parameters = {} if func_parameters is None else func_parameters
+    if as_data_array:
+        return _generate_lazy_data_array(
+            func=func,
+            dims=dims,
+            coords=coords,
+            chunks=chunks,
+            dtype=dtypes,
+            func_parameters=func_parameters
+        )
 
+    return _generate_lazy_dataset(
+        func=func,
+        dims=dims,
+        coords=coords,
+        chunks=chunks,
+        dtypes=dtypes,
+        func_parameters=func_parameters,
+        data_names=data_names
+    )
+
+
+def _chunk_coords(coords, chunks, dims) -> Generator:
+    return itertools.product(*[
+        [coords[dim][i: i + chunk] for i in range(0, len(coords[dim]), chunk)]
+        for chunk, dim in zip(chunks, dims)
+    ])
+
+
+def _generate_lazy_data_array(
+        func: Callable,
+        dims: List[Hashable],
+        coords: Dict[Hashable, Union[List, np.ndarray]],
+        chunks: List[int],
+        dtype: Any,
+        func_parameters: Dict[str, Any],
+):
+    """
+    This method works using the task graph of dask, which is a little bit complex to use, but it reduce the time
+    for generating the array from multiple arrays.
+
+    The important part is that the task graph is a simple dictionary that has as a key the name of the array
+    and the chunk position, so every chunk is contiguos to the other and they follow the order of 0, 1, 2, 3 and so on.
+    for two dimensional cases the positions looks like (0, 0), (0, 1), ..., (N, 0), (N, 1), ... (N, M).
+
+    It's important to keep aligned the chunks coords and the chunks positions, the order is mantained due
+    to the way that itertools product works.
+
+    """
+    task_graph = {}
+    chunks_positions = itertools.product(*[range(ceil(len(coords[dim]) / chunk)) for chunk, dim in zip(chunks, dims)])
+
+    for chunk_coords, chunk_pos in zip(_chunk_coords(coords, chunks, dims), chunks_positions):
+        chunk_coords = {dim: coord for dim, coord in zip(dims, chunk_coords)}
+        parameters = {**func_parameters, **{'coords': chunk_coords}}
+        task_graph[('arr', *chunk_pos)] = (dask.utils.apply, func, [], parameters)
+
+    arr = dask.array.Array(
+        dask=task_graph,
+        name='arr',
+        shape=list(len(coords[dim]) for dim in dims),
+        chunks=chunks,
+        dtype=dtype
+    )
+
+    return xr.DataArray(
+        arr,
+        dims=dims,
+        coords=coords
+    )
+
+
+def _generate_lazy_dataset(
+        func: Callable,
+        dims: List[Hashable],
+        coords: Dict[Hashable, Union[List, np.ndarray]],
+        chunks: List[int],
+        dtypes: List[Any],
+        data_names: List[Hashable],
+        func_parameters: Dict[str, Any],
+):
     chunked_arrays = []
-    for chunk_coords in itertools.product(*total_chunk_coords):
+    for chunk_coords in _chunk_coords(coords, chunks, dims):
         chunk_coords = {dim: coord for dim, coord in zip(dims, chunk_coords)}
         func_parameters['coords'] = chunk_coords
         shape = list(len(chunk_coords[dim]) for dim in dims)
-        if as_data_array:
-            delayed_func = [dask.delayed(func)(**func_parameters)]
-        else:
-            delayed_func = dask.delayed(func, nout=len(data_names))(**func_parameters)
+        delayed_func = dask.delayed(func, nout=len(data_names))(**func_parameters)
         dataset = xr.Dataset({
             name: xr.DataArray(
                 dask.array.from_delayed(
@@ -113,7 +180,5 @@ def defined_translation(
         })
         chunked_arrays.append(dataset)
 
-    dataset = xr.combine_by_coords(chunked_arrays)
-    if as_data_array:
-        return dataset[data_names[0]]
-    return dataset
+    return xr.combine_by_coords(chunked_arrays)
+
