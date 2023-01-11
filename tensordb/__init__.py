c1e4a7b8e0246fc5f5ba9415d9e3b07df3a90b19
Joseph Gonzalez <josephgonowak97@gmail.com>
2023-01-11 13:43:25 -0400

    Adding an interface for the tensor client

--- tensordb/__init__.py
+++ tensordb/__init__.py
@@ -1,7 +1,10 @@
 from . import tensor_definition
 from . import utils
 from .algorithms import Algorithms
-from .file_cache_tensor_client import FileCacheTensorClient
-from .tensor_client import TensorClient
+from .clients import (
+    TensorClient,
+    FileCacheTensorClient,
+    BaseTensorClient
+)
 from .tensor_definition import TensorDefinition
 from .utils.tools import extract_paths_from_formula
