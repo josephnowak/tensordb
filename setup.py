1ef2392b257cda3675288e8c1341ff4725e7969a
Joseph Gonzalez <josephgonowak97@gmail.com>
2023-10-03 16:22:20 -0400

    Improve the performance of the apply_on_groups when using Algorithms methods and change how the unique coords works and change the append method to avoid the use of combine_first

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='TensorDB',
-    version='0.28',
+    version='0.29',
     description='Database based in a file system storage combined with Xarray and Zarr',
     author='Joseph Nowak',
     author_email='josephgonowak97@gmail.com',
