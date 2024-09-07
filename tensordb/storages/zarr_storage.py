781b39fdceeb9bca5a335dce017ec104589ef2db
Joseph Gonzalez <josephgonowak97@gmail.com>
2024-09-07 18:30:05 -0400

    The append and update method were refactored to improve its efficiency and solve issues, now we have a preview method for both, which allows to return the datasets and the necessary metadata to write them, this simplifies a lot the testing process, also many more tests were added to cover the issues found on both methods

--- tensordb/storages/zarr_storage.py
+++ tensordb/storages/zarr_storage.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Union, Any, Literal
+from typing import Dict, List, Union, Any, Literal, Tuple, Hashable
 
 import numpy as np
 import xarray as xr
@@ -128,6 +128,203 @@ class ZarrStorage(BaseStorage):
             new_data = new_data if self.chunks is None else new_data.chunk(self.chunks)
         return new_data
 
+    def append_preview(
+        self, new_data: xr.Dataset, fill_value
+    ) -> Tuple[xr.Dataset, Dict[str | Hashable, xr.Dataset], bool]:
+        """
+        Generates the arrays that must call the to_zarr method with an append_dim.
+
+        Returns
+        -------
+
+        It returns a tuple with three elements:
+
+        1. complete_data: It's the expected results after appending the new data.
+            This dataset is delayed, and it's used to restore the whole data
+             only if the rewrite output is True, and it's also use for testing and debugging purpose
+
+        2. data_to_append: It's a dict whose keys are dimensions and the values are datasets,
+            every dataset is the new data that needs to be persisted in Zarr, it is important to
+            highlight that if there is more than one key on this dict then the to_zarr method
+            must be invoked one time per dataset and in the order of the dimensions
+
+        3. rewrite: It's a bool that indicates if the dataset must be restored or not
+            if True then tensordb must store the dataset again to preserve the order,
+            and it can only be True if and only if there is an insertion in the middle
+            (or at the beginning)
+
+        """
+        act_data = self._transform_to_dataset(self.read(), chunk_data=False)
+        new_data = self._keep_unique_coords(new_data)
+        new_data = self._keep_sorted_coords(new_data)
+        new_data = self._transform_to_dataset(new_data, chunk_data=False)
+
+        self.clear_encoding(new_data)
+
+        # Decide if the data needs to be restored due to insertions in the middle
+        rewrite = False
+        # The data stored here are the new datasets must be rechunked to be aligned
+        # with the chunks of the data already stored.
+        # Note: The order of insertion is defined by the dims of the data
+        data_to_append = {}
+        # The slices are used to calculate the correct chunks to align the data_to_append chunks
+        slices_to_append = {}
+        # The complete_data is a representation of the final array after concatenating the data_to_append
+        # to the act_data, and it is also used to calculate the proper chunks for the data_to_append
+        complete_data = act_data
+
+        for dim in new_data.dims:
+            new_coord = new_data.indexes[dim]
+            act_coord = act_data.indexes[dim]
+            # new elements on the coord to append
+            coord_to_append = new_coord[~new_coord.isin(act_coord)]
+            # Validate if there are new coords to append
+            # TODO: This condition is blocking the use of duplicated coordinates
+            if len(coord_to_append) == 0:
+                continue
+
+            # Check if the data that is going to be inserted respects the sort conditions
+            # (if exists), in case that no, then a rewrite is necessary
+            rewrite |= not self._validate_sorted_append(
+                current_coord=act_coord, append_coord=coord_to_append, dim=dim
+            )
+
+            slices_to_append[dim] = {
+                k: slice(size, None) if k == dim else slice(0, size)
+                for k, size in complete_data.sizes.items()
+            }
+
+            reindex_coords = {
+                k: coord_to_append if k == dim else act_coord
+                for k, act_coord in complete_data.coords.items()
+            }
+
+            # Reindex the new_data to align it with the complete_data
+            data_to_append[dim] = new_data.reindex(
+                reindex_coords, fill_value=fill_value
+            )
+
+            # Append the data in a delayed way to simulate the appending of the data
+            # and generate the proper chunks to match with Zarr
+            complete_data = xr.concat(
+                [complete_data, data_to_append[dim]], dim=dim, fill_value=fill_value
+            )
+
+        # Rechunk the complete_data to make it consistent with the Zarr chunks
+        complete_data = xr.Dataset(
+            {
+                # TODO: On V1 this must be removed in favour of using encoding
+                k: v.chunk(act_data[k].encoding["preferred_chunks"])
+                for k, v in complete_data.items()
+            }
+        )
+
+        if rewrite:
+            # If rewrite is necessary then resort, drop duplicates on the coords and rechunk it
+            complete_data = self._keep_unique_coords(complete_data)
+            complete_data = self._keep_sorted_coords(complete_data)
+            complete_data = self._transform_to_dataset(complete_data)
+            # Not necessary, return an empty dict to avoid confusion
+            data_to_append = {}
+        else:
+            # Rechunk the data_to_append in the dim order based on the chunks of the complete_data
+            # after slicing it, this is to avoid calculating the correct chunks by hand.
+            data_to_append = {
+                dim: data_to_append[dim].chunk(
+                    complete_data.isel(**slices_to_append[dim]).chunksizes
+                )
+                for dim, data in data_to_append.items()
+            }
+
+        return complete_data, data_to_append, rewrite
+
+    def update_preview(
+        self,
+        new_data: xr.Dataset,
+        complete_update_dims: str | List[str] | None,
+        fill_value: Any,
+    ):
+        """
+        Generates the arrays that must call the to_zarr method with the regions parameter.
+
+        Returns
+        -------
+
+        It returns a tuple with two elements:
+
+        1. update_data: The dataset that contains the new and act values that are inside
+            the regions boundary defined by new_data. The chunks are already aligned
+
+        2. regions: Region at which the update_data must be inserted in the Zarr store.
+        """
+        act_data = self._transform_to_dataset(self.read(), chunk_data=False)
+        new_data = self._transform_to_dataset(new_data, chunk_data=False)
+        new_data = self._keep_unique_coords(new_data)
+        new_data = self._keep_sorted_coords(new_data)
+
+        self.clear_encoding(new_data)
+
+        act_coords = {k: coord for k, coord in act_data.coords.items()}
+
+        # The new data must contain only coordinates that are on the act_coords
+        new_data = new_data.sel(
+            {k: new_data.coords[k].isin(v) for k, v in act_coords.items()}
+        )
+        if any(size == 0 for size in new_data.sizes.values()):
+            return xr.Dataset(), {}
+
+        if complete_update_dims is not None:
+            if isinstance(complete_update_dims, str):
+                complete_update_dims = [complete_update_dims]
+
+            new_data = new_data.reindex(
+                **{
+                    dim: coord
+                    for dim, coord in act_coords.items()
+                    if dim in complete_update_dims
+                },
+                fill_value=fill_value,
+            )
+
+        regions = {}
+        for coord_name in act_data.dims:
+            act_bitmask = act_coords[coord_name].isin(
+                new_data.coords[coord_name].values
+            )
+            valid_positions = np.nonzero(act_bitmask.values)[0]
+            regions[coord_name] = slice(
+                np.min(valid_positions), np.max(valid_positions) + 1
+            )
+
+        act_data_region = act_data.isel(**regions)
+
+        # The bitmask_arr identifies which cells of the act_data_region needs to be updated
+        # with the new_data, the final shape is equal to the act_data_region
+        bitmask_arr = None
+        for dim, coord in act_data_region.coords.items():
+            # Get which coords needs to be updated for this dimension and chunk it properly
+            bitmask_coord = coord.isin(new_data.coords[dim]).chunk(
+                {dim: act_data_region.chunksizes[dim]}
+            )
+            if bitmask_arr is None:
+                bitmask_arr = bitmask_coord
+            else:
+                # Concat a 1D array (called A) with another array (call it B) with different dims
+                # is going to generate a cartesian product:
+                # B.dims[0] x  B.dims[1] x ...  B.dims[N] x A.dims[0]
+                bitmask_arr = bitmask_arr & bitmask_coord
+
+        # reindex the new data to be aligned with the region
+        new_data = new_data.reindex(act_data_region.coords, fill_value=fill_value)
+
+        # The chunks must match with the chunks of the actual data after applying the region slice
+        new_data = new_data.chunk(act_data_region.chunksizes)
+
+        # Only update the corresponding cells
+        update_data = new_data.where(bitmask_arr, act_data_region)
+
+        return update_data, regions
+
     @staticmethod
     def clear_encoding(dataset):
         # TODO: Once https://github.com/pydata/xarray/issues/4380 is fixed delete the temporal solution of encoding
@@ -248,51 +445,10 @@ class ZarrStorage(BaseStorage):
         if not self.exist():
             return [self.store(new_data=new_data, compute=compute)]
 
-        act_data = self._transform_to_dataset(self.read(), chunk_data=False)
-        new_data = self._keep_unique_coords(new_data)
-        new_data = self._keep_sorted_coords(new_data)
-        new_data = self._transform_to_dataset(new_data, chunk_data=False)
-
-        self.clear_encoding(new_data)
-
-        rewrite = False
-        act_coords = {k: coord for k, coord in act_data.indexes.items()}
-        data_to_append = {}
-        slices_to_append = {}
-        complete_data = act_data
-
-        for dim in new_data.dims:
-            new_coord = new_data.indexes[dim]
-            act_coord = act_coords[dim]
-            coord_to_append = new_coord[~new_coord.isin(act_coord)]
-            if len(coord_to_append) == 0:
-                continue
-
-            rewrite |= not self._validate_sorted_append(
-                current_coord=act_coord, append_coord=coord_to_append, dim=dim
-            )
-
-            reindex_coords = {
-                k: coord_to_append if k == dim else act_coord
-                for k, act_coord in complete_data.coords.items()
-            }
-            slices_to_append[dim] = {
-                k: slice(size, None) if k == dim else slice(0, size)
-                for k, size in complete_data.sizes.items()
-            }
-            data_to_append[dim] = new_data.reindex(
-                reindex_coords, fill_value=fill_value
-            )
-            complete_data = xr.concat(
-                [complete_data, data_to_append[dim]], dim=dim, fill_value=fill_value
-            )
-
-        complete_data = xr.Dataset(
-            {
-                k: v.chunk(act_data[k].encoding["preferred_chunks"])
-                for k, v in complete_data.items()
-            }
+        complete_data, data_to_append, rewrite = self.append_preview(
+            new_data=new_data, fill_value=fill_value
         )
+
         if rewrite:
             return [self.store(new_data=complete_data, compute=compute, rewrite=True)]
 
@@ -300,25 +456,23 @@ class ZarrStorage(BaseStorage):
         #   without a synchronizer even if they are executed one after the other, or apparently
         #   I'm not using properly the bind function of Dask, so for now set to compute as True
         #   if there is two or more dims to append
-        if len(slices_to_append) > 1 and self.synchronizer is None:
+        if len(data_to_append) > 1 and self.synchronizer is None:
             compute = True
 
         delayed_appends = []
         for dim in new_data.dims:
-            if dim not in slices_to_append:
+            if dim not in data_to_append:
                 continue
 
-            data = data_to_append[dim].chunk(
-                complete_data.isel(**slices_to_append[dim]).chunks
-            )
             delayed_appends.append(
-                data.to_zarr(
+                data_to_append[dim].to_zarr(
                     self.base_map,
                     append_dim=dim,
                     compute=compute,
                     synchronizer=self.synchronizer,
                     consolidated=True,
                     group=self.group,
+                    # TODO: Send an MR to Xarray solving the incorrect detection of chunks
                     safe_chunks=False,
                 )
             )
@@ -330,6 +484,7 @@ class ZarrStorage(BaseStorage):
         new_data: Union[xr.DataArray, xr.Dataset],
         compute: bool = True,
         complete_update_dims: Union[List[str], str] = None,
+        fill_value: Any = np.nan,
     ) -> Union[xr.backends.ZarrStore, None]:
         """
         Replace data on an existing Zarr files based on the new_data, internally calls the method
@@ -353,6 +508,8 @@ class ZarrStorage(BaseStorage):
         compute: bool, default True
             Same meaning that in xarray
 
+        fill_value: Any, default np.nan
+
         Returns
         -------
 
@@ -360,57 +517,21 @@ class ZarrStorage(BaseStorage):
         `to_zarr <https://xr.pydata.org/en/stable/generated/xr.Dataset.to_zarr.html>`_
         """
 
-        act_data = self._transform_to_dataset(self.read(), chunk_data=False)
-        new_data = self._transform_to_dataset(new_data, chunk_data=False)
-        new_data = self._keep_unique_coords(new_data)
-        new_data = self._keep_sorted_coords(new_data)
-
-        self.clear_encoding(new_data)
-
-        act_coords = {k: coord for k, coord in act_data.coords.items()}
-
-        # The new data must contain only coordinates that are on the act_coords
-        new_data = new_data.sel(
-            {k: new_data.coords[k].isin(v) for k, v in act_coords.items()}
+        update_data, regions = self.update_preview(
+            new_data=new_data,
+            fill_value=fill_value,
+            complete_update_dims=complete_update_dims,
         )
-        if any(size == 0 for size in new_data.sizes.values()):
+        if not regions:
             return None
 
-        if complete_update_dims is not None:
-            if isinstance(complete_update_dims, str):
-                complete_update_dims = [complete_update_dims]
-            new_data = new_data.reindex(
-                **{
-                    dim: coord
-                    for dim, coord in act_coords.items()
-                    if dim in complete_update_dims
-                }
-            )
-
-        regions = {}
-        for coord_name in act_data.dims:
-            act_bitmask = act_coords[coord_name].isin(
-                new_data.coords[coord_name].values
-            )
-            valid_positions = np.nonzero(act_bitmask.values)[0]
-            regions[coord_name] = slice(
-                np.min(valid_positions), np.max(valid_positions) + 1
-            )
-
-        act_data_region = act_data.isel(**regions)
-        if complete_update_dims is None:
-            new_data = new_data.combine_first(act_data_region)
-
-        # The chunks must match with the chunks of the actual data after applying the region slice
-        new_data = new_data.chunk(act_data_region.chunksizes)
-
-        delayed_write = new_data.to_zarr(
+        delayed_write = update_data.to_zarr(
             self.base_map,
             group=self.group,
             compute=compute,
             synchronizer=self.synchronizer,
             region=regions,
-            # This option is save based on this https://github.com/pydata/xarray/issues/9072
+            # This option is safe based on this https://github.com/pydata/xarray/issues/9072
             safe_chunks=False,
         )
         return delayed_write
@@ -504,7 +625,7 @@ class ZarrStorage(BaseStorage):
 
         Returns
         -------
-        True if the tensor exist, False if it not exist
+        True if the tensor exist, False if it does not exist
 
         """
         try:
